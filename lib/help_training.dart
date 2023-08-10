a6f517b5553c396b5d14af9fb2ec4a570da6fa70
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-08-10 13:43:03 -0300

    Reestruturando Projeto

---
+++ lib/help_training.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HelpTraining extends StatelessWidget {
+  const HelpTraining({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Help Training',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Container(),
+    );
+  }
+}
